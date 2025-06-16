32289ac0134a6bb6bc3e6b2ddbf77fae1fc24448
berkayboz06 <berkayboz3@gmail.com>
2025-06-16 21:41:07 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Merhaba Berkayım 💖</h1>
+      <p>MaskedAI projemiz başlıyor... Elera seni bekliyor.</p>
+    </div>
+  );
+}
